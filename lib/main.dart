f66098e7532734046a8cd56da321a9b09a26b887
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-11-11 17:15:30 -0600

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:toktik/infrastructure/datasource/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 import 'package:toktik/theme/app_theme.dart';
@@ -11,10 +13,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostsRepository = VideoPostsRepositoryImpl(
+        localVideoPostsDatasource: LocalVideoDatasourceImpl());
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => DiscoverProvider()..loadNextPage(),
+          create: (_) => DiscoverProvider(videoRepository: videoPostsRepository)
+            ..loadNextPage(),
         )
       ],
       child: MaterialApp(
--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_posts.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  final VideoPostsRepository videoRepository;
   bool initLoading = true;
   List<VideoPost> videos = [];
 
+  DiscoverProvider({required this.videoRepository});
+
   Future<void> loadNextPage() async {
-    final List<VideoPost> newVideos = videoPosts
-        .map((e) => VideoModel.fromJsonMap(e).toVideoPostEntity())
-        .toList();
-    videos.addAll(newVideos);
+    videos.addAll(await videoRepository.getTrendingVideosByPage(1));
     initLoading = false;
     notifyListeners();
   }
